1c44088612c6a7c862dcdc9d3d780f8d47b77f86
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-05-05 21:14:16 +0200

    added button for oppening the chat

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -4,6 +4,11 @@ import ChatBot from "./ChatBot";
 
 function ProductList() {
 
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleChat = () => {
+        setIsOpen(!isOpen);
+    };
+
     const [items, setItems] = useState([]);
     async function pobieranieitemow() {
     try {
@@ -23,7 +28,7 @@ function ProductList() {
     }, []);
 
     return (
-        <div className="max-w-5xl mx-auto my-8 px-4 absolute">
+        <div className="max-w-5xl mx-auto my-8 px-4 static">
             <h2 className="text-2xl font-bold mb-6 text-center">Product List</h2>
             <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {items.map(item => (
@@ -38,8 +43,12 @@ function ProductList() {
                     </Link>
                 ))}
             </ul>
-            <div class="absolute bottom-0 right-0"> 
-            <ChatBot /> 
+            <div class="absolute bottom-0 right-0 my-2 px-4">  
+            {isOpen ? (
+                <ChatBot />
+            ) : (
+                <button onClick={toggleChat} className="bg-green-500 text-black px-4 py-2 rounded">OPEN CHAT</button>
+            )}
             </div>
         </div>
     );
